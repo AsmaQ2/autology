939fd992cdbbdae848cf4d66d9d10a378ecdd5d8
AsmaQ2 <103947419+AsmaQ2@users.noreply.github.com>
2022-04-18 20:49:33 +0400

    Update alpha_1.py

--- alpha_1.py
+++ alpha_1.py
@@ -1,52 +1,259 @@
+import docx
+import os
+def queue_text(seq, useq):
+    if seq == 1:
+        for root, dirs, files in os.walk('D:/un/diss/project/test_text/1'):
+            for file in files:
+                doc = docx.Document(r"D:/un/diss/project/test_text/1/"+file)
+                y = 0
+                txt5 = ''
+                for paragraph in doc.paragraphs:
+                    for run in paragraph.runs:
+                        txt = run.text
+                        txt2=txt.lower()
+                        txt3=txt2.replace(",", "")
+                        txt4 = txt3.replace(".", "")
+                        txt5=txt4.strip()
+                        print(txt5)
+                        print(y)
+                        if run.bold:
+                            print('1')
+                            a = str(run.text)
+                            g = "'"+a+"'"
+                            query1 = "MERGE(f:Fish{name:"+g+"})"
+                            conn.query(query1, db='DB1')
+                        if run.italic:
+                            print('2')
+                            q = str(run.text)
+                            t = "'"+q+"'"
+                            query1 = "MATCH(f:Fish{name:"+g+"}) MERGE(d:Dome{name:"+t+"}) MERGE (f)-[:isa]->(d)"
+                            conn.query(query1, db='DB1')
+                        if txt5 in ('усов', 'усы'):
+                            y = 1
+                            print('a')
+                            txt5 = ''
+                        if run.underline and y == 1:
+                            print('3')
+                            с = str(run.text)
+                            h = "'"+с+"'"
+                            query1 = "MATCH(f:Fish{name:"+g+"}) SET f.barbel="+h
+                            conn.query(query1, db='DB1')
+                            y = 0
+                        if txt5 =='губы':
+                            y = 2
+                            print('b')
+                            txt5 = ''
+                        if run.underline and y == 2:
+                            print('4')
+                            с = str(run.text)
+                            h = "'"+с+"'"
+                            query1 = "MATCH(f:Fish{name:"+g+"}) SET f.lips="+h
+                            conn.query(query1, db='DB1')
+                            y = 0
+                        if txt5 == 'размер':
+                            y = 3
+                            print('c')
+                            txt5 = ''
+                        if run.underline and y == 3:
+                            print('5')
+                            с = str(run.text)
+                            h = "'"+с+"'"
+                            query1 = "MATCH(f:Fish{name:"+g+"}) SET f.size="+h
+                            conn.query(query1, db='DB1')
+                            y = 0
+    if seq == 2:
+        for root, dirs, files in os.walk('D:/un/diss/project/test_text/2'):
+            for file in files:
+                doc = docx.Document(r"D:/un/diss/project/test_text/2/"+file)
+                y = 0
+                txt5 = ''
+                for paragraph in doc.paragraphs:
+                    for run in paragraph.runs:
+                        txt = run.text
+                        txt2=txt.lower()
+                        txt3=txt2.replace(",", "")
+                        txt4 = txt3.replace(".", "")
+                        txt5=txt4.strip()
+                        print(txt5)
+                        print(y)
+                        if run.bold:
+                            print('1')
+                            a = str(run.text)
+                            g = "'"+a+"'"
+                            query1 = "MERGE(f:Fish{name:"+g+"})"
+                            conn.query(query1, db='DB1')
+                        if run.italic:
+                            print('2')
+                            q = str(run.text)
+                            t = "'"+q+"'"
+                            query1 = "MATCH(f:Fish{name:"+g+"}) MERGE(d:Dome{name:"+t+"}) MERGE (f)-[:isa]->(d)"
+                            conn.query(query1, db='DB1')
+                        if txt5 in ('усов', 'усы'):
+                            y = 1
+                            print('a')
+                            txt5 = ''
+                        if run.underline and y == 1:
+                            print('3')
+                            с = str(run.text)
+                            h = "'"+с+"'"
+                            query1 = "MATCH(f:Fish{name:"+g+"}) SET f.barbel="+h
+                            conn.query(query1, db='DB1')
+                            y = 0
+                        if txt5 =='губы':
+                            y = 2
+                            print('b')
+                            txt5 = ''
+                        if run.underline and y == 2:
+                            print('4')
+                            с = str(run.text)
+                            h = "'"+с+"'"
+                            query1 = "MATCH(f:Fish{name:"+g+"}) SET f.lips="+h
+                            conn.query(query1, db='DB1')
+                            y = 0
+                        if txt5 == 'размер':
+                            y = 3
+                            print('c')
+                            txt5 = ''
+                        if run.underline and y == 3:
+                            print('5')
+                            с = str(run.text)
+                            h = "'"+с+"'"
+                            query1 = "MATCH(f:Fish{name:"+g+"}) SET f.size="+h
+                            conn.query(query1, db='DB1')
+                            y = 0
+    if seq == 3:
+        for root, dirs, files in os.walk('D:/un/diss/project/test_text/3'):
+            for file in files:
+                doc = docx.Document(r"D:/un/diss/project/test_text/3/"+file)
+                y = 0
+                txt5 = ''
+                for paragraph in doc.paragraphs:
+                    for run in paragraph.runs:
+                        txt = run.text
+                        txt2=txt.lower()
+                        txt3=txt2.replace(",", "")
+                        txt4 = txt3.replace(".", "")
+                        txt5=txt4.strip()
+                        print(txt5)
+                        print(y)
+                        if run.bold:
+                            print('1')
+                            a = str(run.text)
+                            g = "'"+a+"'"
+                            query1 = "MERGE(f:Fish{name:"+g+"})"
+                            conn.query(query1, db='DB1')
+                        if run.italic:
+                            print('2')
+                            q = str(run.text)
+                            t = "'"+q+"'"
+                            query1 = "MATCH(f:Fish{name:"+g+"}) MERGE(d:Dome{name:"+t+"}) MERGE (f)-[:isa]->(d)"
+                            conn.query(query1, db='DB1')
+                        if txt5 in ('усов', 'усы'):
+                            y = 1
+                            print('a')
+                            txt5 = ''
+                        if run.underline and y == 1:
+                            print('3')
+                            с = str(run.text)
+                            h = "'"+с+"'"
+                            query1 = "MATCH(f:Fish{name:"+g+"}) SET f.barbel="+h
+                            conn.query(query1, db='DB1')
+                            y = 0
+                        if txt5 =='губы':
+                            y = 2
+                            print('b')
+                            txt5 = ''
+                        if run.underline and y == 2:
+                            print('4')
+                            с = str(run.text)
+                            h = "'"+с+"'"
+                            query1 = "MATCH(f:Fish{name:"+g+"}) SET f.lips="+h
+                            conn.query(query1, db='DB1')
+                            y = 0
+                        if txt5 == 'размер':
+                            y = 3
+                            print('c')
+                            txt5 = ''
+                        if run.underline and y == 3:
+                            print('5')
+                            с = str(run.text)
+                            h = "'"+с+"'"
+                            query1 = "MATCH(f:Fish{name:"+g+"}) SET f.size="+h
+                            conn.query(query1, db='DB1')
+                            y = 0
+    if seq == 4:
+        for root, dirs, files in os.walk('D:/un/diss/project/test_text/4'):
+            for file in files:
+                doc = docx.Document(r"D:/un/diss/project/test_text/4/"+file)
+                y = 0
+                txt5 = ''
+                for paragraph in doc.paragraphs:
+                    for run in paragraph.runs:
+                        txt = run.text
+                        txt2=txt.lower()
+                        txt3=txt2.replace(",", "")
+                        txt4 = txt3.replace(".", "")
+                        txt5=txt4.strip()
+                        print(txt5)
+                        print(y)
+                        if run.bold:
+                            print('1')
+                            a = str(run.text)
+                            g = "'"+a+"'"
+                            query1 = "MERGE(f:Fish{name:"+g+"})"
+                            conn.query(query1, db='DB1')
+                        if run.italic:
+                            print('2')
+                            q = str(run.text)
+                            t = "'"+q+"'"
+                            query1 = "MATCH(f:Fish{name:"+g+"}) MERGE(d:Dome{name:"+t+"}) MERGE (f)-[:isa]->(d)"
+                            conn.query(query1, db='DB1')
+                        if txt5 in ('усов', 'усы'):
+                            y = 1
+                            print('a')
+                            txt5 = ''
+                        if run.underline and y == 1:
+                            print('3')
+                            с = str(run.text)
+                            h = "'"+с+"'"
+                            query1 = "MATCH(f:Fish{name:"+g+"}) SET f.barbel="+h
+                            conn.query(query1, db='DB1')
+                            y = 0
+                        if txt5 =='губы':
+                            y = 2
+                            print('b')
+                            txt5 = ''
+                        if run.underline and y == 2:
+                            print('4')
+                            с = str(run.text)
+                            h = "'"+с+"'"
+                            query1 = "MATCH(f:Fish{name:"+g+"}) SET f.lips="+h
+                            conn.query(query1, db='DB1')
+                            y = 0
+                        if txt5 == 'размер':
+                            y = 3
+                            print('c')
+                            txt5 = ''
+                        if run.underline and y == 3:
+                            print('5')
+                            с = str(run.text)
+                            h = "'"+с+"'"
+                            query1 = "MATCH(f:Fish{name:"+g+"}) SET f.size="+h
+                            conn.query(query1, db='DB1')
+                            y = 0
+    print('done')
+    return seq
 
-os.chdir(r"D:\un\diss\project")
-def queue_text(seq):
-    list_queue = list(range(1, x)) #изменить X на количество файлов в папке
-    for i in list_queue:
-        if seq == 1:
-            for root, dirs, files in os.walk('D:/un/diss/project/comp_science'):
-                for file in files:
-                    doc = docx.Document(r"D:/un/diss/project/comp_science/"+file)
-                    y = 0
-                    txt5 = ''
-                    for paragraph in doc.paragraphs:
-                        for run in paragraph.runs:
-                            txt = run.text
-                            txt2=txt.lower()
-                            txt3=txt2.replace(",", "")
-                            txt4 = txt3.replace(".", "")
-                            txt5=txt4.strip()
-                            if run.bold:
-                                main_term = str(run.text)
-                                main_term_name = "'"+main_term+"'"
-                                query1 = "MERGE(t:Term{name:"+main_term_name+"})"
-                                conn.query(query1, db='DB1')
-                                receiver = main_term_name
-                                instance_receiver = main_term_name
-                            if run.italic and run.underline:
-                                host_term = str(run.text)
-                                host_term_name = "'"+host_term+"'"
-                                query1 = "MATCH(t:Term{name:"+main_term_name+"}) MERGE(s:Term{name:"+host_term_name+"}) MERGE (t)-[:is_a]->(s)"
-                                conn.query(query1, db='DB1')
-                            if txt5 in ('англ'):
-                                syn = 1
-                                txt5 = ''
-                            if run.italic and syn == 1:
-                                sub_term = str(run.text)
-                                sub_term_name = "'"+sub_term+"'"
-                                query1 = "MATCH(t:Term{name:"+main_term_name+"}) MERGE(s:Term{name:"+sub_term_name+"}) MERGE (t)-[:synonym]->(s)"
-                                conn.query(query1, db='DB1')
-                                syn = 0
-                            if run.italic and not run.underline:
-                                sub_term = str(run.text)
-                                sub_term_name = "'"+sub_term+"'"
-                                query1 = "MATCH(t:Term{name:"+main_term_name+"}) MERGE(s:Term{name:"+sub_term_name+"}) MERGE (s)-[:is_part_of]->(t)"
-                                conn.query(query1, db='DB1')
-                                receiver = sub_term_name
-                                instance_receiver = sub_term_name
-                            if run.underline and not run.italic:
-                                instance = str(run.text)
-                                instance_name = "'"+instance+"'"
-                                query1 = "MERGE(i:Instance{name:"+instance_name+"}) MATCH (t:Term{name:"+instance_receiver+"}) MERGE (i)-[:is_implementation]->(t)
-                                conn.query(query1, db='DB1')
-
+import threading
+import os
+import time
+if __name__ == '__main__':
+    start_time = time.clock()
+    p1 = threading.Thread(target = queue_text, args =(1, '' ) ) 
+    p1.start()
+    p2 = threading.Thread(target = queue_text, args =(2, '' ) ) 
+    p2.start()
+    p3 = threading.Thread(target = queue_text, args =(3, '' ) ) 
+    p3.start()
+    p4 = threading.Thread(target = queue_text, args =(4, '' ) ) 
+    p4.start()
